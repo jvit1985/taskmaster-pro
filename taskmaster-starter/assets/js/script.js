88ceac24fa48e37a732db3730be3d14c64d09208
Justin Vititoe <jvit1985@yahoo.com>
2022-03-23 20:13:40 -0500

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -66,6 +66,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <=2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 $(".list-group").on("click", "p", function () {
@@ -174,7 +175,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -210,16 +211,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   update: function() {
     //array to store the task data in
@@ -258,19 +261,25 @@ $(".card .list-group").sortable({
     tolerance: "touch",
     drop: function(event, ui) {
       ui.draggable.remove();
-      console.log("drop");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
-    over: function(event,ui) {
-      console.log("over");
+    over: function(event, ui) {
+      $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui) {
-      console.log("out");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     }
   });
 
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
 // load tasks for the first time
